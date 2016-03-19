14c0c7396239b4edb2f2c7e8792c0c61cd44d9ab
Emobe <anthony.r.shuker@gmail.com>
2016-03-19 01:56:37 +0000

    dfgdf

--- server.js
+++ server.js
@@ -2,35 +2,14 @@
 const express = require('express');
 const fs = require('fs');
 const crypto = require('crypto');
-const auth = require('basic-auth');
-const user_1 = require('./models/user');
 const mongoose = require('mongoose');
 let app = express();
 mongoose.connect(process.env.MONGOCON);
 app.listen(1339, err => {
     console.log('media stream started');
 });
-app.get('/media/:type/:fileId/:userId', (req, res) => {
+app.get('/media/:type/:fileId/:user/:userIdId', (req, res) => {
     let cred;
-    try {
-        cred = auth(req);
-    }
-    catch (err) {
-        return res.status(401).end();
-    }
-    if (cred.name.length && cred.pass.length) {
-        user_1.user.findOne({ _id: cred.name, apiKey: cred.pass }, (err, user) => {
-            if (user) {
-                req.body.userId = user._id.toString();
-            }
-            else {
-                return res.status(401).end();
-            }
-        });
-    }
-    else {
-        return res.status(401).end();
-    }
     let fileId, userId, type, path;
     try {
         fileId = req.params.fileId;
